568f20b7ff2c7e5e9349090f7aaa530200cc5184
Khaled Monsoor <k@kmonsoor.com>
2016-01-11 03:42:34 +0600

    implement twitter embed

--- embed_x.py
+++ embed_x.py
@@ -2,23 +2,20 @@ from urlparse import urlparse, parse_qs
 
 
 class OnlineContent(object):
-    url = None
-    content_uid = None
     instance = None
 
     # these templates must be defined by providing subclasses
-    EMBED_SCRIPT = ''''''
-    LINK_TEMPLATE = ''''''
-    STATUS_LINK = ''''''
+    hostnames = []
+    EMBED_SCRIPT = ""
+    LINK_TEMPLATE = ""
+    STATUS_LINK = ""
 
     def __init__(self, url):
-        self.url = url
-        # URL-specific dispatching to `provider` objects e.g. Youtube, Vimeo etc.
+        # URL-specific dispatching to `provider` objects e.g. Youtube, Twitter etc.
         for provider_obj in self.__class__.__subclasses__():
-            _hosts = provider_obj.get_hostnames()
-            if any( x for x in _hosts if x in url):
-                self.instance = provider_obj(self.url)
-                self.content_uid = self.get_content_uid()
+            _hosts = provider_obj.hostnames
+            if any(x for x in _hosts if x in url):
+                self.instance = provider_obj(url)
 
     def get_provider(self):
         return self.instance.__str__()
@@ -26,22 +23,17 @@ class OnlineContent(object):
     def get_content_uid(self):
         """Returns the unique ID extracted from the link of the content
         """
-        self.content_uid = self.instance.extract_id()
-        return self.content_uid
+        return self.instance.extract_id()
 
     def get_embed_code(self):
-        if self.content_uid is None:
-            self.instance.extract_id()
-        if len(self.instance.EMBED_SCRIPT) > 0:
-            return self.instance.EMBED_SCRIPT % ({'content_uid': self.content_uid})
+        if len(self.instance.EMBED_SCRIPT):
+            return self.instance.EMBED_SCRIPT % ({'content_uid': self.get_content_uid()})
         else:
             raise NotImplementedError
 
     def get_clean_link(self):
-        if self.content_uid is None:
-            self.instance.extract_id()
-        if len(self.instance.LINK_TEMPLATE) > 0:
-            return self.instance.LINK_TEMPLATE % ({'content_uid': self.content_uid})
+        if len(self.instance.LINK_TEMPLATE):
+            return self.instance.LINK_TEMPLATE % ({'content_uid': self.get_content_uid()})
         else:
             raise NotImplementedError
 
@@ -59,29 +51,29 @@ class OnlineContent(object):
         """
         from urllib2 import urlopen, URLError, HTTPError
 
-        if not len(self.instance.STATUS_LINK):
-            raise NotImplementedError
-        url = self.instance.STATUS_LINK % ({'content_uid': self.content_uid})
+        if len(self.instance.STATUS_LINK):
+            check_url = self.instance.STATUS_LINK % ({'content_uid': self.get_content_uid()})
+        else:
+            # fallback
+            check_url = self.instance.url
+
         try:
-            response = urlopen(url)
+            response = urlopen(check_url)
         except (HTTPError, URLError):
             return False
         except ValueError:
-            raise URLError('Invalid URL: %s'.format(url))
+            raise URLError('Invalid URL: %s'.format(check_url))
         else:
             return True if response.code == 200 else False
 
 
 class YouTube(OnlineContent):
-    # hostnames = ['youtube', 'youtu.be']
-    EMBED_SCRIPT = '''<div class='embed-container'><iframe src='http://www.youtube.com/embed/%(content_uid)s'
-                      'frameborder='0'allowfullscreen></iframe></div>'''
+    hostnames = ['youtube', 'youtu.be']
+    EMBED_SCRIPT = ("<div class='embedx-yt'>"
+                    "<iframe src='http://www.youtube.com/embed/%(content_uid)s' 'frameborder='0' allowfullscreen>"
+                    "</iframe></div>")
     STATUS_LINK = '''http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=%(content_uid)s&format=json'''
     LINK_TEMPLATE = '''https://www.youtube.com/watch?v=%(content_uid)s'''
-
-    @staticmethod
-    def get_hostnames():
-        return ['youtube', 'youtu.be']
 
     def __init__(self, url):
         self.url = url
@@ -100,7 +92,7 @@ class YouTube(OnlineContent):
           'youtube.com/watch?v=_lOT2p_FCvA',
 
           Invalid:
-            'youtu.be/watch?v=_lOT2p_FCvA',
+          'youtu.be/watch?v=_lOT2p_FCvA',
         """
 
         if self.url.startswith(('youtu', 'www')):
@@ -119,13 +111,14 @@ class YouTube(OnlineContent):
 
 
 class Vimeo(OnlineContent):
+    hostnames = ['vimeo', ]
+
     LINK_TEMPLATE = '''https://vimeo.com/%(content_uid)s'''
     STATUS_LINK = '''https://vimeo.com/api/oembed.json?url=https://vimeo.com/%(content_uid)s'''
-    EMBED_SCRIPT = '''<div class='embed-container'> <iframe src='http://player.vimeo.com/video/%(content_uid)s' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>'''
-
-    @staticmethod
-    def get_hostnames():
-        return ['vimeo', ]
+    EMBED_SCRIPT = ("<div class='embedx-vm'>"
+                    "<iframe src='http://player.vimeo.com/video/%(content_uid)s' "
+                    "frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen>"
+                    "</iframe></div>")
 
     def __init__(self, url):
         self.url = url
@@ -137,9 +130,64 @@ class Vimeo(OnlineContent):
             return self.url.split('/')[-1]
 
 
-if __name__ == '__main__':
-    ov = OnlineContent(url='https://vimeo.com/groups/animation/videos/150618894/')
+class Twitter(OnlineContent):
+    hostnames = ['twitter', ]
 
-    print ov.get_content_uid()
-    print ov.check_if_alive()
-    print ov.get_embed_code()
+    LINK_TEMPLATE = ""
+    STATUS_LINK = ""
+    EMBED_SCRIPT = ("<div id='embedx-twt' align='center'></div>"
+                    "<script async src='https://platform.twitter.com/widgets.js'></script>"
+                    "<script> window.onload=(function(){twttr.widgets.createTweet(%(content_uid)s,"
+                    " document.getElementById('embedx-twt'),{});});</script>")
+
+    def __init__(self, url):
+        self.url = url
+
+    @staticmethod
+    def get_hostnames():
+        return ['twitter', ]
+
+    def extract_id(self):
+        if '/status/' not in self.url:
+            raise NotImplementedError
+        else:
+            return urlparse(self.url).path.split('/')[3]
+
+
+class Flickr(OnlineContent):
+    hostnames = ['flickr', ]
+
+    LINK_TEMPLATE = ""
+    STATUS_LINK = ""
+    EMBED_SCRIPT = ""
+
+    def extract_id(self):
+        raise NotImplementedError
+
+
+class Facebook(OnlineContent):
+    hostnames = ['facebook', 'fb.com']
+
+    LINK_TEMPLATE = ""
+    STATUS_LINK = ""
+    EMBED_SCRIPT = ""
+
+    def extract_id(self):
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    test_urls = ['https://twitter.com/thepodcastdude/status/686258030229336064',
+                 'youtube.com/watch?v=_lOT2p_FCvA',
+                 'https://www.facebook.com/kmonsoor/posts/10153282994792374',
+                 'vimeo.com/150519302'
+                 ]
+
+    for a_url in test_urls:
+        try:
+            ov = OnlineContent(a_url)
+            print ov.get_content_uid()
+            # print ov.check_if_alive()
+            print ov.get_embed_code()
+        except NotImplementedError:
+            pass
