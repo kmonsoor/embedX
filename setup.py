56aa8b7c681430045bcdf4f490cfea5ce855699e
Khaled Monsoor <k@kmonsoor.com>
2016-01-15 01:01:06 +0600

    typo in module name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except(IOError, ImportError):
     long_description = open('README.md').read()
 
 setup(
-        name='embedX',
+        name='embedx',
         version=__version__,
         packages=find_packages(),
         url=__source_url__,
